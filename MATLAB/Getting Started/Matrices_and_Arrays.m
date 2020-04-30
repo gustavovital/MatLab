4de195ba1916ccc2405c934079178c833ace3c43
Gustavo Vital <gustavovital@id.uff.br>
2020-04-30 01:14:35 -0300

    first commit

---
+++ MATLAB/Getting Started/Matrices_and_Arrays.m
@@ -0,0 +1,22 @@
+% Criação de arrays
+
+ a = [1 2 3 4]
+
+% Uma matriz
+
+a = [1 2 3; 4 5 6; 7 8 10]
+
+% E criando uma matriz de zeros
+
+disp('E criando uma matriz de zeros');
+zeros(5,5)
+
+disp('Ou uma de zeros 5 por 1');
+
+zeros(5,1)
+
+disp('Soamndo a com 10');
+a + 10
+
+disp('Ou o seno de a:');
+sin(a)
