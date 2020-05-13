6d09ae5e5b818086dee013e0cf5b3f0f4d93f567
Gustavo Vital <gustavovital@id.uff.br>
2020-05-12 23:54:29 -0300

    update LA

--- MATLAB/Linear Algebra/Matrix_Operations.m
+++ MATLAB/Linear Algebra/Matrix_Operations.m
@@ -1,4 +1,25 @@
 clc; clear all
 slCharacterEncoding('UTF-8');
 
+disp('Matriz simetrica');
 A = pascal(3)
+
+disp('Matriz assimetrica');
+B = magic(3)
+
+disp('Matriz 3x2 de inteiros aleatorios');
+C = fix(10*rand(3, 2))
+
+disp('Vetores e linhas');
+u = [3; 1; 4]
+v = [2 0 -1]
+s = 7
+
+disp('----------------------------------------------------');
+disp('Operações entre matrizes');
+disp('');
+disp('X = A + B');
+X = A + B
+disp('');
+disp('Y = X - A');
+Y = X - A
