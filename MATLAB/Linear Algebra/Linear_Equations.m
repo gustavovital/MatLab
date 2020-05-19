60bebfd3c4624ec2e99a3607731164ca2274337f
Gustavo Vital <gustavovital@id.uff.br>
2020-05-19 17:10:59 -0300

    just a test

--- MATLAB/Linear Algebra/Linear_Equations.m
+++ MATLAB/Linear Algebra/Linear_Equations.m
@@ -26,3 +26,20 @@ A*pinv(A)*b
 disp('Redução para determinar solução exata ou não, por meio do algoritimo');
 disp('de echelon. rref([A b])');
 rref([A b])
+
+disp('Em relaçao a overdetermined systems');
+disp('suponha t e y para a seguinte função:');
+disp('y(t) = c1 + c2exp(-t)');
+t = [0 .3 .8 1.1 1.6 2.3]';
+y = [.82 .72 .63 .6 .55 .5]';
+B = table(t, y)
+
+disp('E segue a tentativa de tentar modelar..');
+disp('E = [ones(size(t)) exp(-t)]');
+E = [ones(size(t)) exp(-t)]
+disp('E com c = E\y temos as constantes');
+c = E\y
+
+T = (0:0.1:2.5)';
+Y = [ones(size(T)) exp(-T)]*c;
+plot(T, Y, '-', t, y, 'o')
