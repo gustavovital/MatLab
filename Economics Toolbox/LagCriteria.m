5b1fef9bfd34fae8b650a502e0e83e1b1f591ba9
Gustavo de Oliveira <gustavovital@id.uff.br>
2020-07-27 00:38:45 -0300

    add lag criteria

--- Economics Toolbox/LagCriteria.m
+++ Economics Toolbox/LagCriteria.m
@@ -1,4 +1,4 @@
-function [display] = LagCriteria(VAR)
+function [Table] = LagCriteria(VAR)
 
     
     % iterations of lags
@@ -6,13 +6,35 @@ function [display] = LagCriteria(VAR)
        
     
     numberOfVar = size(VAR, 2);
-    
+    AIC = [];
+    BIC = [];
+    LogLikelihood = [];
     for i=1:defaultIter
-        
         baselineModel = varm(numberOfVar, i);
         summary = summarize(estimate(baselineModel, VAR));
-        display = [summary.AIC summary.BIC summary.LogLikelihood];
-       
-    end    
-    display;
+        AIC = [AIC; summary.AIC];
+        BIC = [BIC; summary.BIC];
+        LogLikelihood  = [LogLikelihood; summary.LogLikelihood];
+        
+    end  
+    
+    aicmin = min(AIC);
+    bicmin = min(BIC);
+    llmax  = max(LogLikelihood);
+    
+%     for value=1:defaultIter
+%         if AIC(value) == aicmin
+%             AIC(value) = strcat(num2str(AIC(value)), "*");
+%         end
+%         if BIC(value) == bicmin
+%             BIC(value) = strcat(num2str(BIC(value)), "*");
+%         end
+%         if LogLikelihood(value) == llmax
+%             LogLikelihood(value) = strcat(num2str(LogLikelihood(value)), "*");
+%         end
+%     end
+
+    display = table(AIC, BIC, LogLikelihood);
+    disp(display);
+      
 end
