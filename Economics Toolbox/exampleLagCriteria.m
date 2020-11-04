f1babbc171e42f0f4bf7a6befff568d1ab053c57
Gustavo de Oliveira <gustavovital@id.uff.br>
2020-11-04 23:06:03 +0000

    moved toolbox

--- Economics Toolbox/exampleLagCriteria.m
+++
@@ -1,8 +0,0 @@
-%% Data
-load Data_USEconModel
-rcpi = price2ret(DataTable.CPIAUCSL);
-unrate = DataTable.UNRATE(2:end);
-
-%% Select lag from VAR
-LagCriteria([rcpi unrate], 6)
-% LagCriteria([rcpi unrate])
